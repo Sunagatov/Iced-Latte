c5df324739a1fed507d117a473690e071d0ab951
Zufar Sunagatov <zufar.sunagatov@gmail.com>
2022-12-09 14:27:23 +0000

    Configuration and main class were added.

---
+++ src/main/java/com/zufarproject/aws/AwsApplication.java
@@ -0,0 +1,13 @@
+package com.zufarproject.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zufarproject/aws/AwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zufarproject.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
