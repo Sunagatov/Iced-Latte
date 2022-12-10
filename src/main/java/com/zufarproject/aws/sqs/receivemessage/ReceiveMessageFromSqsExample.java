b318d06a40ef24f238c8e9a47c5fc34a67306260
zufar-sunagatov <zufar.sunagatov@gmail.com>
2022-12-10 17:25:19 +0000

    com.zufarproject.aws.example was renamed to com.zufarproject.aws.sqs

--- src/main/java/com/zufarproject/aws/sqs/receivemessage/ReceiveMessageFromSqsExample.java
+++ src/main/java/com/zufarproject/aws/sqs/receivemessage/ReceiveMessageFromSqsExample.java
@@ -1,4 +1,4 @@
-package com.zufarproject.aws.example.sendmessage;
+package com.zufarproject.aws.sqs.receivemessage;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
@@ -6,29 +6,26 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
-import com.amazonaws.services.sqs.model.SendMessageBatchRequest;
-import com.amazonaws.services.sqs.model.SendMessageBatchRequestEntry;
+import com.amazonaws.services.sqs.model.Message;
 
-import java.util.Set;
+import java.util.List;
 
-public class SendBatchMessageToSqsQueueExample {
+public class ReceiveMessageFromSqsExample {
     private static final String QUEUE_URL = System.getProperty("AWS_QUEUE_URL");
     private static final String AWS_ACCESS_KEY = System.getProperty("AWS_ACCESS_KEY");
     private static final String AWS_SECRET_KEY = System.getProperty("AWS_SECRET_KEY");
     private static final AWSCredentials CREDENTIALS = new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY);
 
     public static void main(String[] args) {
-        final SendMessageBatchRequest messageBatchRequest = new SendMessageBatchRequest()
-                .withQueueUrl(QUEUE_URL)
-                .withEntries(Set.of(new SendMessageBatchRequestEntry("msg_1", "Hello from message 1"),
-                                new SendMessageBatchRequestEntry("msg_2", "Hello from message 2").withDelaySeconds(3)
-                ));
-
         final AmazonSQS sqsClient = AmazonSQSClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(CREDENTIALS))
                 .withRegion(Regions.DEFAULT_REGION)
                 .build();
 
-        sqsClient.sendMessageBatch(messageBatchRequest);
+        List<Message> messages = sqsClient
+                .receiveMessage(QUEUE_URL)
+                .getMessages();
+
+        messages.forEach(message -> System.out.println("***************" + message.getBody()));
     }
 }
--- src/main/java/com/zufarproject/aws/sqs/sendmessage/SendMessageToSqsQueueExample.java
+++ src/main/java/com/zufarproject/aws/sqs/sendmessage/SendMessageToSqsQueueExample.java
@@ -1,4 +1,4 @@
-package com.zufarproject.aws.example.receivemessage;
+package com.zufarproject.aws.sqs.sendmessage;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
@@ -6,26 +6,25 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
-import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
 
-import java.util.List;
-
-public class ReceiveMessageFromSqsExample {
+public class SendMessageToSqsQueueExample {
     private static final String QUEUE_URL = System.getProperty("AWS_QUEUE_URL");
     private static final String AWS_ACCESS_KEY = System.getProperty("AWS_ACCESS_KEY");
     private static final String AWS_SECRET_KEY = System.getProperty("AWS_SECRET_KEY");
     private static final AWSCredentials CREDENTIALS = new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY);
 
     public static void main(String[] args) {
+        final SendMessageRequest sendMessageRequest = new SendMessageRequest()
+                .withQueueUrl(QUEUE_URL)
+                .withMessageBody("Hi Zufar!")
+                .withDelaySeconds(5);
+
         final AmazonSQS sqsClient = AmazonSQSClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(CREDENTIALS))
                 .withRegion(Regions.DEFAULT_REGION)
                 .build();
 
-        List<Message> messages = sqsClient
-                .receiveMessage(QUEUE_URL)
-                .getMessages();
-
-        messages.forEach(message -> System.out.println("***************" + message.getBody()));
+        sqsClient.sendMessage(sendMessageRequest);
     }
 }
