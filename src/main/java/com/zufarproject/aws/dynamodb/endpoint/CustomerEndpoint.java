74073b5e7b1f959ca4f429366125a509194490c8
Zufar Sunagatov <zufar.sunagatov@gmail.com>
2022-12-14 14:51:43 +0000

    ResponseBody was added in CustomerEndpoint.

--- src/main/java/com/zufarproject/aws/dynamodb/endpoint/CustomerEndpoint.java
+++ src/main/java/com/zufarproject/aws/dynamodb/endpoint/CustomerEndpoint.java
@@ -3,15 +3,17 @@ package com.zufarproject.aws.dynamodb.endpoint;
 import com.zufarproject.aws.dynamodb.model.Customer;
 import com.zufarproject.aws.dynamodb.repository.CustomerCrudRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class CustomerEndpoint {
-
-    private final CustomerCrudRepository customerCrudRepository;
+    private CustomerCrudRepository customerCrudRepository;
 
     @PostMapping("/customer")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
     public Customer saveCustomer(@RequestBody final Customer customer) {
         return customerCrudRepository.saveCustomer(customer);
     }
@@ -23,12 +25,16 @@ public class CustomerEndpoint {
     }
 
     @DeleteMapping("/customer/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
     public String deleteCustomerById(@PathVariable("id") final String customerId) {
         customerCrudRepository.deleteCustomerById(customerId);
         return  "";
     }
 
     @PutMapping("/customer/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
     public String updateCustomer(@PathVariable("id") final String customerId,
                                  @RequestBody final Customer customer) {
         customerCrudRepository.updateCustomer(customerId,customer);
