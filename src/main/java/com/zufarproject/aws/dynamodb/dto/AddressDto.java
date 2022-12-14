89bf7b494d9947a5ff47b7008344cf071c4a33b0
Zufar Sunagatov <zufar.sunagatov@gmail.com>
2022-12-14 16:03:07 +0000

    Validation Size annotations were added.

--- src/main/java/com/zufarproject/aws/dynamodb/dto/AddressDto.java
+++ src/main/java/com/zufarproject/aws/dynamodb/dto/AddressDto.java
@@ -1,5 +1,6 @@
 package com.zufarproject.aws.dynamodb.dto;
 
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,10 +10,13 @@ import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBl
 @AllArgsConstructor
 @NoArgsConstructor
 public class AddressDto {
-    @NotBlank(message = "line is mandatory")
+    @NotBlank(message = "Line must be between 2 to 55 characters")
+    @Size(min = 2, max = 55)
     private String line;
-    @NotBlank(message = "city is mandatory")
+    @NotBlank(message = "City must be between 2 to 55 characters")
+    @Size(min = 2, max = 55)
     private String city;
-    @NotBlank(message = "country is mandatory")
+    @NotBlank(message = "Country must be between 2 to 55 characters")
+    @Size(min = 2, max = 55)
     private String country;
 }
--- src/main/java/com/zufarproject/aws/dynamodb/dto/CustomerDto.java
+++ src/main/java/com/zufarproject/aws/dynamodb/dto/CustomerDto.java
@@ -1,7 +1,9 @@
 package com.zufarproject.aws.dynamodb.dto;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,15 +13,18 @@ import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBl
 @AllArgsConstructor
 @NoArgsConstructor
 public class CustomerDto {
-    @NotBlank(message = "customerId is mandatory")
+    @NotBlank(message = "CustomerId is mandatory")
     private String customerId;
-    @NotBlank(message = "firstName is mandatory")
+    @NotBlank(message = "FirstName must be between 2 to 55 characters")
+    @Size(min = 2, max = 55)
     private String firstName;
-    @NotBlank(message = "lastName is mandatory")
+    @NotBlank(message = "LastName must be between 2 to 55 characters")
+    @Size(min = 2, max = 55)
     private String lastName;
-    @NotBlank(message = "email is mandatory")
+    @NotBlank(message = "Email is mandatory")
+    @Email
     private String email;
-    @NotNull(message = "addressDto is mandatory")
+    @NotNull(message = "AddressDto is mandatory")
     @Valid
     private AddressDto addressDto;
 }
