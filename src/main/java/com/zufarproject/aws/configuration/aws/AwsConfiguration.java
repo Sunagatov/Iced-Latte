513eebee617f909c88e6efa793aa463c32491357
Zufar Sunagatov <zufar.sunagatov@gmail.com>
2023-01-03 13:10:08 +0000

    AwsConfiguration Getter annotation was added.

--- src/main/java/com/zufarproject/aws/configuration/aws/AwsConfiguration.java
+++ src/main/java/com/zufarproject/aws/configuration/aws/AwsConfiguration.java
@@ -10,10 +10,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.Getter;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 
+@Getter
 @Configuration
 public class AwsConfiguration {
 
