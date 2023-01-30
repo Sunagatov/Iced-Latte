536c7af2a960902f61b15c0653fee2b1aa541c5e
Zufar Sunagatov <zufar.sunagatov@gmail.com>
2023-01-30 14:52:22 +0000

    Products for mongodb were updated.

--- src/main/java/com/zufar/onlinestore/configuration/mongo/MongoConfig.java
+++ src/main/java/com/zufar/onlinestore/configuration/mongo/MongoConfig.java
@@ -7,11 +7,9 @@ import com.zufar.onlinestore.repository.ProductInfoRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import java.math.BigDecimal;
 
-@EnableMongoRepositories(basePackageClasses = ProductInfoRepository.class)
 @Configuration
 public class MongoConfig {
 
@@ -21,24 +19,24 @@ public class MongoConfig {
 			repository.save(
 					new ProductInfo(
 							1,
-							"Product2",
+							"Product1",
 							new PriceDto(BigDecimal.valueOf(256, 7), "GB"),
 							"Category1"));
 			repository.save(
 					new ProductInfo(
-							1,
+							2,
 							"Product2",
 							new PriceDto(BigDecimal.valueOf(312, 7), "GB"),
 							"Category1"));
 			repository.save(
 					new ProductInfo(
-							1,
+							3,
 							"Product3",
 							new PriceDto(BigDecimal.valueOf(123, 7), "GB"),
 							"Category2"));
 			repository.save(
 					new ProductInfo(
-							1,
+							4,
 							"Product4",
 							new PriceDto(BigDecimal.valueOf(223, 7), "GB"),
 							"Category2"));
