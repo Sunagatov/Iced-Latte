1e78835d0e47cc55fa5e9164bbb0ce0df02036af
Zufar Sunagatov <zufar.sunagatov@gmail.com>
2023-01-30 14:53:48 +0000

    @EnableMongoRepositories annotation was added to OnlineStoreApplication class.

--- src/main/java/com/zufar/onlinestore/OnlineStoreApplication.java
+++ src/main/java/com/zufar/onlinestore/OnlineStoreApplication.java
@@ -2,7 +2,9 @@ package com.zufar.onlinestore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoRepositories
 @SpringBootApplication
 public class OnlineStoreApplication {
 
