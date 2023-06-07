a7a527bfd8fd22deacf21fd588de272b3797e862
KalachyanD <kalachyand@gmail.com>
2023-06-07 20:38:37 +0400

    Code formatting fix

--- src/main/java/com/zufar/onlinestore/security/configuration/SpringSecurityConfiguration.java
+++ src/main/java/com/zufar/onlinestore/security/configuration/SpringSecurityConfiguration.java
@@ -23,49 +23,50 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SpringSecurityConfiguration {
-	private static final String API_AUTH_URL_PREFIX = "/api/auth/**";
+    private static final String API_AUTH_URL_PREFIX = "/api/auth/**";
     private static final String API_DOCS_URL_PREFIX = "/api/docs/**";
-	public static final String ACTUATOR_ENDPOINTS_URL_PREFIX = "/actuator/**";
+    public static final String ACTUATOR_ENDPOINTS_URL_PREFIX = "/actuator/**";
 
-	private final UserDetailsServiceImpl userDetailsService;
-	private final JwtAuthenticationFilter jwtTokenFilter;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final JwtAuthenticationFilter jwtTokenFilter;
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(final HttpSecurity httpSecurity) throws Exception {
-		return httpSecurity
-				.csrf().disable()
-				.authorizeHttpRequests()
-				.requestMatchers(API_AUTH_URL_PREFIX).permitAll()
+    @Bean
+    public SecurityFilterChain securityFilterChain(final HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers(API_AUTH_URL_PREFIX).permitAll()
                 .requestMatchers(API_DOCS_URL_PREFIX).permitAll()
-				.requestMatchers(ACTUATOR_ENDPOINTS_URL_PREFIX).permitAll()
-				.anyRequest().authenticated()
-				.and()
-				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-				.and()
-				.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
-				.build();
-	}
+                .requestMatchers(ACTUATOR_ENDPOINTS_URL_PREFIX).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 
-	@Bean
-	public AuthenticationProvider authenticationProvider() {
-		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-		authenticationProvider.setUserDetailsService(userDetailsService);
-		authenticationProvider.setPasswordEncoder(passwordEncoder());
-		return authenticationProvider;
-	}
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
 
-	@Bean
-	public AuthenticationManager authenticationManager(final HttpSecurity httpSecurity,
-	                                                   final PasswordEncoder passwordEncoder,
-	                                                   final UserDetailsServiceImpl userDetailService) throws Exception {
-		return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
-				.userDetailsService(userDetailService)
-				.passwordEncoder(passwordEncoder)
-				.and()
-				.build();
-	}
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    public AuthenticationManager authenticationManager(final HttpSecurity httpSecurity,
+                                                       final PasswordEncoder passwordEncoder,
+                                                       final UserDetailsServiceImpl userDetailService) throws Exception {
+        return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailService)
+                .passwordEncoder(passwordEncoder)
+                .and()
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
