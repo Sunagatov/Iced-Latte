3f62895aee23a44ccb976dae82a2863e054e9ce1
UladzislauM <marshal25@yandex.by>
2023-07-16 15:30:52 +0300

    Product: Change NUMERIC to DECIMAL

--- src/main/resources/db/changelog/product.sql
+++ src/main/resources/db/changelog/product.sql
@@ -2,7 +2,7 @@ CREATE TABLE product(
     id          UUID PRIMARY KEY,
     name        VARCHAR(64) NOT NULL,
     description TEXT,
-    price       NUMERIC     NOT NULL CHECK (price > 0),
+    price       DECIMAL     NOT NULL CHECK (price > 0),
     currency    VARCHAR     NOT NULL,
     quantity    INT         NOT NULL CHECK (quantity >= 0),
     active      BOOLEAN     NOT NULL
