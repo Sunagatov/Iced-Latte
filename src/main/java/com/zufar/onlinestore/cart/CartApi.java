f8674001ca8108c7d1919b1cf771e88e0b5c5540
zufar-sunagatov <zufar.sunagatov@gmail.com>
2023-07-19 14:33:42 +0100

    CartApi was added

---
+++ src/main/java/com/zufar/onlinestore/cart/CartApi.java
@@ -0,0 +1,42 @@
+package com.zufar.onlinestore.cart;
+
+import com.zufar.onlinestore.cart.dto.AddNewItemToShoppingSessionRequest;
+import com.zufar.onlinestore.cart.dto.GetShoppingSessionRequest;
+import com.zufar.onlinestore.cart.dto.RemoveItemFromShoppingSessionRequest;
+import com.zufar.onlinestore.cart.dto.ShoppingSession;
+import com.zufar.onlinestore.cart.dto.UpdateProductAmountInShoppingSessionItemRequest;
+
+public interface CartApi {
+
+    /**
+     * Метод получения покупочной сессии (текущее состояние корзины)
+     *
+     * @param getShoppingSessionRequest запрос на получение покупочной сессии (текущее состояние корзины)
+     * @return покупочная сессия (текущее состояние корзины)
+     * */
+    ShoppingSession getShoppingSession(final GetShoppingSessionRequest getShoppingSessionRequest);
+
+    /**
+     * Метод добавление нового item в покупочную сессию (в корзины)
+     *
+     * @param addNewItemToShoppingSessionRequest запрос на добавление нового item в покупочную сессию (в корзины)
+     * @return покупочная сессия (текущее состояние корзины)
+     * */
+    ShoppingSession addNewItemToShoppingSession(final AddNewItemToShoppingSessionRequest addNewItemToShoppingSessionRequest);
+
+    /**
+     * Метод удаления существующего item в покупочной сессии (в корзине)
+     *
+     * @param removeItemFromShoppingSessionRequest запрос на удаления существующего item в покупочной сессии (в корзине)
+     * @return покупочная сессия (текущее состояние корзины)
+     * */
+    ShoppingSession removeItemFromShoppingSession(final RemoveItemFromShoppingSessionRequest removeItemFromShoppingSessionRequest);
+
+    /**
+     * Метод изменения количества продуктов в определенном item в покупочной сессии (в корзине)
+     *
+     * @param updateProductAmountInShoppingSessionItemRequest запрос на изменение количества продуктов в определенном item в покупочной сессии (в корзине)
+     * @return покупочная сессия (текущее состояние корзины)
+     * */
+    ShoppingSession updateProductAmountInShoppingSessionItem(final UpdateProductAmountInShoppingSessionItemRequest updateProductAmountInShoppingSessionItemRequest);
+}
