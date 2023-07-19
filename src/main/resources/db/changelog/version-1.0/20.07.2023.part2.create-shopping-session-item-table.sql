4bd11546580a45837db90858af36607fd2b0334a
Zufar Sunagatov <zufar.sunagatov@gmail.com>
2023-07-19 15:37:40 +0100

    [cart-1] Create cart and cart item database tables  (#54)
    * 20.07.2023.part1.create-cart-table.sql and 20.07.2023.part2.create-cart-item-table.sql were added
    
    * Added some new constraints for timestamp attributes.

---
+++ src/main/resources/db/changelog/version-1.0/20.07.2023.part2.create-shopping-session-item-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS shopping_session_item
+(
+    id                 UUID        PRIMARY KEY,
+    cart_id            UUID        NOT NULL,
+    product_id         UUID        NOT NULL,
+    products_quantity  INT         NOT NULL CHECK (products_quantity >= 0)
+)
