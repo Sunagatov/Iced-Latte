31de13927d379a092c4e82c810141655690f2b1c
zufar-sunagatov <zufar.sunagatov@gmail.com>
2023-07-21 10:22:46 +0100

    CartApi was updated

--- src/main/java/com/zufar/onlinestore/cart/api/CartApi.java
+++ src/main/java/com/zufar/onlinestore/cart/api/CartApi.java
@@ -9,34 +9,34 @@ import com.zufar.onlinestore.cart.dto.UpdateProductsQuantityInShoppingSessionIte
 public interface CartApi {
 
     /**
-     * Enables to get ShoppingSession (the Cart details)
+     * Enables to get ShoppingSession (the cart details)
      *
-     * @param getShoppingSessionRequest the request to get the shopping session (the shoppingSession)
-     * @return Shopping session (the shoppingSession details)
+     * @param getShoppingSessionRequest the request to get the shopping session (the cart details)
+     * @return Shopping session (the cart details)
      * */
     ShoppingSessionDto getShoppingSession(final GetShoppingSessionRequest getShoppingSessionRequest);
 
     /**
-     * Enables to add a new item into the shopping session (the shoppingSession)
+     * Enables to add a new item into the shopping session (the cart details)
      *
-     * @param addNewItemToShoppingSessionRequest the request to add a new item into the shopping session (the shoppingSession)
-     * @return Shopping session (the shoppingSession details)
+     * @param addNewItemToShoppingSessionRequest the request to add a new item into the shopping session (the cart details)
+     * @return Shopping session (the cart details)
      * */
     ShoppingSessionDto addNewItemToShoppingSession(final AddNewItemToShoppingSessionRequest addNewItemToShoppingSessionRequest);
 
     /**
-     * Enables to remove the specific item from the shopping session (the shoppingSession)
+     * Enables to remove the specific item from the shopping session (the cart details)
      *
-     * @param removeItemFromShoppingSessionRequest the request to remove the specific item from the shopping session (the shoppingSession)
+     * @param removeItemFromShoppingSessionRequest the request to remove the specific item from the shopping session (the cart details)
      * @return Shopping session (the shoppingSession details)
      * */
     ShoppingSessionDto removeItemFromShoppingSession(final RemoveItemFromShoppingSessionRequest removeItemFromShoppingSessionRequest);
 
     /**
-     * Enables to change the product's quantity in the specific item of the shopping session (the shoppingSession)
+     * Enables to change the product's quantity in the specific item of the shopping session (the cart details)
      *
-     * @param updateProductsQuantityInShoppingSessionItemRequest the request to change the product's quantity in the specific item of the shopping session (the shoppingSession)
-     * @return Shopping session (the shoppingSession details)
+     * @param updateProductsQuantityInShoppingSessionItemRequest the request to change the product's quantity in the specific item of the shopping session (the cart details)
+     * @return Shopping session (the cart details)
      * */
     ShoppingSessionDto updateProductAmountInShoppingSessionItem(final UpdateProductsQuantityInShoppingSessionItemRequest updateProductsQuantityInShoppingSessionItemRequest);
 }
