4cc1dfb6fffb723cd8e6777e2f79e5be4c61fe62
zufar-sunagatov <zufar.sunagatov@gmail.com>
2023-07-21 02:03:20 +0100

    Unused import was deleted

--- src/main/java/com/zufar/onlinestore/cart/endpoint/CartEndpoint.java
+++ src/main/java/com/zufar/onlinestore/cart/endpoint/CartEndpoint.java
@@ -8,14 +8,20 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @Validated
-@RequestMapping(value = "/api/cart")
+@RequestMapping(value = CartEndpoint.CART_URL)
 public class CartEndpoint {
+
+    public static final String CART_URL = "/api/v1/cart";
 
     private final CartApi cartApi;
 
