e9411c0275f2b60e86327fa43c9deeae9e133e09
zufar-sunagatov <zufar.sunagatov@gmail.com>
2023-07-21 02:03:05 +0100

    Unused import was deleted

--- src/main/java/com/zufar/onlinestore/cart/repository/ShoppingSessionRepository.java
+++ src/main/java/com/zufar/onlinestore/cart/repository/ShoppingSessionRepository.java
@@ -2,7 +2,6 @@ package com.zufar.onlinestore.cart.repository;
 
 import com.zufar.onlinestore.cart.entity.ShoppingSession;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
