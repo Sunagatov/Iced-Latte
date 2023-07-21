0cddd67792fdfac2429834e60df77c62569f0e24
zufar-sunagatov <zufar.sunagatov@gmail.com>
2023-07-21 01:30:40 +0100

    Insert statements were added

--- src/main/resources/db/changelog/version-1.0/21.07.2023.part2.create-shopping-session-item-table.sql
+++ src/main/resources/db/changelog/version-1.0/21.07.2023.part2.create-shopping-session-item-table.sql
@@ -1,16 +1,7 @@
 CREATE TABLE IF NOT EXISTS shopping_session_item
 (
     id                 UUID        PRIMARY KEY,
-    cart_id            UUID        NOT NULL,
+    shopping_session_id            UUID        NOT NULL,
     product_id         UUID        NOT NULL,
     products_quantity  INT         NOT NULL CHECK (products_quantity >= 0)
 )
-
-Корзины
-(
-    Collection (Items)
-)
-
-Items (
-    Item <-
-      )
