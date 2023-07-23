3e13e792bdbdaf083068b92674a3111f673fe6e3
zufar-sunagatov <zufar.sunagatov@gmail.com>
2023-07-24 00:24:41 +0100

    Payment package classes were refactored a little bit

--- src/main/java/com/zufar/onlinestore/payment/repository/PaymentRepository.java
+++ src/main/java/com/zufar/onlinestore/payment/repository/PaymentRepository.java
@@ -1,6 +1,6 @@
 package com.zufar.onlinestore.payment.repository;
 
-import com.zufar.onlinestore.payment.model.Payment;
+import com.zufar.onlinestore.payment.entity.Payment;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PaymentRepository extends CrudRepository<Payment, Long> {
