49a2915d7f1f7dccfad294373a9fc0ec87245130
UladzislauM <marshal25@yandex.by>
2023-07-24 11:21:45 +0300

    product-1: changed the type of id to UUID

--- src/main/java/com/zufar/onlinestore/product/repository/ProductInfoRepository.java
+++ src/main/java/com/zufar/onlinestore/product/repository/ProductInfoRepository.java
@@ -1,10 +1,11 @@
 package com.zufar.onlinestore.product.repository;
 
 import com.zufar.onlinestore.product.entity.ProductInfo;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface ProductInfoRepository extends JpaRepository<ProductInfo, Integer> {
+public interface ProductInfoRepository extends JpaRepository<ProductInfo, UUID> {
 }
