afe7cc615367a58ab8a9ec98c44b8b1f850cfcd4
zufar-sunagatov <zufar.sunagatov@gmail.com>
2023-07-26 22:31:12 +0100

    User was updated

--- src/main/java/com/zufar/onlinestore/user/dto/UserDto.java
+++ src/main/java/com/zufar/onlinestore/user/dto/UserDto.java
@@ -8,11 +8,13 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
+import java.util.UUID;
+
 @Builder
 public record UserDto(
 
         @Nullable
-        String userId,
+        UUID userId,
 
         @NotBlank(message = "FirstName is the mandatory attribute")
         @Size(max = 55, message = "FirstName length must be less than 55 characters")
