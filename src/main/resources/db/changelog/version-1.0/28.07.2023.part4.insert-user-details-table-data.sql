c0bd6994c4c8d2510e39e28114eb951ae924e161
zufar-sunagatov <zufar.sunagatov@gmail.com>
2023-07-27 03:41:23 +0100

    all tested data for address and user_details tables were added

---
+++ src/main/resources/db/changelog/version-1.0/28.07.2023.part4.insert-user-details-table-data.sql
@@ -0,0 +1,18 @@
+-- Inserts for the "user_details" table with associated addresses
+INSERT INTO user_details (id, first_name, last_name, user_name, email, password, address_id) VALUES
+    ('11111111-1111-1111-1111-111111111111', 'John', 'Doe', 'johndoe', 'john@example.com', 'pass123', 'a1a1a1a1-a1a1-a1a1-a1a1-a1a1a1a1a1a1');
+
+INSERT INTO user_details (id, first_name, last_name, user_name, email, password, address_id) VALUES
+    ('22222222-2222-2222-2222-222222222222', 'Jane', 'Smith', 'janesmith', 'jane@example.com', 'pass456', 'b2b2b2b2-b2b2-b2b2-b2b2-b2b2b2b2b2b2');
+
+INSERT INTO user_details (id, first_name, last_name, user_name, email, password, address_id) VALUES
+    ('33333333-3333-3333-3333-333333333333', 'Michael', 'Johnson', 'michaelj', 'michael@example.com', 'pass789', 'c3c3c3c3-c3c3-c3c3-c3c3-c3c3c3c3c3c3');
+
+INSERT INTO user_details (id, first_name, last_name, user_name, email, password, address_id) VALUES
+    ('44444444-4444-4444-4444-444444444444', 'Emma', 'Williams', 'emmaw', 'emma@example.com', 'passabc', 'd4d4d4d4-d4d4-d4d4-d4d4-d4d4d4d4d4d4');
+
+INSERT INTO user_details (id, first_name, last_name, user_name, email, password, address_id) VALUES
+    ('55555555-5555-5555-5555-555555555555', 'David', 'Brown', 'davidb', 'david@example.com', 'passxyz', 'e5e5e5e5-e5e5-e5e5-e5e5-e5e5e5e5e5e5');
+
+INSERT INTO user_details (id, first_name, last_name, user_name, email, password, address_id) VALUES
+    ('66666666-6666-6666-6666-666666666666', 'Angelina', 'Jolie', 'angelina', 'angelina@example.com', 'pass111', 'f6f6f6f6-f6f6-f6f6-f6f6-f6f6f6f6f6f6');
