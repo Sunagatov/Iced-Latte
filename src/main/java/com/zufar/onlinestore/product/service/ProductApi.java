c20c908ad4292c13962424435d6c5cc064f9cbae
UladzislauM <marshal25@yandex.by>
2023-07-30 18:41:40 +0300

    product-4: deleted pageable and add several parameters

--- src/main/java/com/zufar/onlinestore/product/service/ProductApi.java
+++ src/main/java/com/zufar/onlinestore/product/service/ProductApi.java
@@ -2,8 +2,6 @@ package com.zufar.onlinestore.product.service;
 
 import com.zufar.onlinestore.product.dto.ProductPaginationDto;
 
-import java.util.UUID;
-
 public interface ProductApi {
 
     ProductPaginationDto getProducts(Integer page,
