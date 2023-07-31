691b7e62d288e29c6b361244f9ea28d3e31c6ccd
UladzislauM <marshal25@yandex.by>
2023-07-31 09:31:09 +0300

    product-6: changed in ProductsEndpoint (getProduct) in compliance with the docs

--- src/main/java/com/zufar/onlinestore/product/endpoint/ProductsEndpoint.java
+++ src/main/java/com/zufar/onlinestore/product/endpoint/ProductsEndpoint.java
@@ -1,10 +1,7 @@
 package com.zufar.onlinestore.product.endpoint;
 
 import com.zufar.onlinestore.product.dto.ProductPaginationDto;
-import com.zufar.onlinestore.product.mapper.ProductInfoDtoConverter;
-import com.zufar.onlinestore.product.dto.ProductInfoDto;
-import com.zufar.onlinestore.product.entity.ProductInfo;
-import com.zufar.onlinestore.product.repository.ProductInfoRepository;
+import com.zufar.onlinestore.product.dto.ProductResponseDto;
 
 import com.zufar.onlinestore.product.service.ProductApi;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
 import java.util.UUID;
 
 import lombok.RequiredArgsConstructor;
@@ -28,23 +24,13 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping(value = "/api/products")
 public class ProductsEndpoint {
 
-    private final ProductInfoRepository productInfoRepository;
-    private final ProductInfoDtoConverter productInfoDtoConverter;
     private final ProductApi productInfoService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<ProductInfoDto> getProductInfoById(@PathVariable("id") final String id) {
-        log.info("Received request to get the ProductInfo with id - {}.", id);
-        Optional<ProductInfo> ProductInfo = productInfoRepository.findById(UUID.fromString(id));
-        if (ProductInfo.isEmpty()) {
-            log.info("the ProductInfo with id - {} is absent.", id);
-            return ResponseEntity.notFound()
-                    .build();
-        }
-        ProductInfoDto ProductInfoDto = productInfoDtoConverter.toDto(ProductInfo.get());
-        log.info("the ProductInfo with id - {} was retrieved - {}.", id, ProductInfoDto);
+    public ResponseEntity<ProductResponseDto> getProductById(@PathVariable("id") final String id) {
+        log.info("Received request to get the Product with id - {}.", id);
         return ResponseEntity.ok()
-                .body(ProductInfoDto);
+                .body(productInfoService.getProduct(UUID.fromString(id)));
     }
 
     @GetMapping
