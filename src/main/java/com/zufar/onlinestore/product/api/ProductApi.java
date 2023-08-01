13ba5bda26f278341473eb3c6145fc6e24d986c2
UladzislauM <marshal25@yandex.by>
2023-08-01 17:18:28 +0300

    product-6: added API description

--- src/main/java/com/zufar/onlinestore/product/api/ProductApi.java
+++ src/main/java/com/zufar/onlinestore/product/api/ProductApi.java
@@ -7,10 +7,26 @@ import java.util.UUID;
 
 public interface ProductApi {
 
+    /**
+     * Enables to get a list of all products sorted with the ability to change the output parameters.
+     *
+     * @param page page number in order (starting from 0)
+     * @param size number of products in the output
+     * @param sortAttribute the name of the product field by which they should be sorted
+     * @param sortDirection sorting direction (DESC or ASC)
+     * @return ProductPaginationDto the sorted list of products includes:
+     * page number, number of products per page, total number of products, total number of pages.
+     */
     ProductPaginationDto getAllProducts(Integer page,
                                         Integer size,
                                         String sortAttribute,
                                         String sortDirection);
 
+    /**
+     * Enables to get a product by its id.
+     *
+     * @param id product id.
+     * @return ProductResponseDto a product with the required fields.
+     */
     ProductResponseDto getProduct(UUID id);
 }
