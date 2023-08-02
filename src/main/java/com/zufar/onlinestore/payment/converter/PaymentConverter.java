0d7777dd6233c5701a02f57e6a473723c72ee0ea
Vitalii <vitaliibredun@gmail.com>
2023-08-02 19:06:11 +0300

    Replaced converter classes of Payment package with Mapstruct

--- src/main/java/com/zufar/onlinestore/payment/converter/PaymentConverter.java
+++ src/main/java/com/zufar/onlinestore/payment/converter/PaymentConverter.java
@@ -2,31 +2,12 @@ package com.zufar.onlinestore.payment.converter;
 
 import com.zufar.onlinestore.payment.dto.PaymentDetailsDto;
 import com.zufar.onlinestore.payment.entity.Payment;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 
-@Component
-public class PaymentConverter {
+@Mapper
+public interface PaymentConverter {
 
-    public PaymentDetailsDto toDto(final Payment entity) {
-        return PaymentDetailsDto.builder()
-                .paymentId(entity.getPaymentId())
-                .paymentIntentId(entity.getPaymentIntentId())
-                .totalPrice(entity.getItemsTotalPrice())
-                .currency(entity.getCurrency())
-                .description(entity.getDescription())
-                .status(entity.getStatus())
-                .build();
-    }
+    PaymentDetailsDto toDto(final Payment entity);
 
-    public Payment toEntity(final PaymentDetailsDto dto) {
-        return Payment.builder()
-                .paymentId(dto.paymentId())
-                .paymentIntentId(dto.paymentIntentId())
-                .itemsTotalPrice(dto.totalPrice())
-                .currency(dto.currency())
-                .description(dto.description())
-                .status(dto.status())
-                .build();
-    }
-
+    Payment toEntity(final PaymentDetailsDto dto);
 }
