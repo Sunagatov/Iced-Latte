7bb6f1bd24020dfea4b4931e535c59df1649310f
zufar-sunagatov <zufar.sunagatov@gmail.com>
2023-08-09 22:33:38 +0100

    All code smells which sonarqube found were fixed

--- src/main/java/com/zufar/onlinestore/security/configuration/SpringSecurityConfiguration.java
+++ src/main/java/com/zufar/onlinestore/security/configuration/SpringSecurityConfiguration.java
@@ -16,7 +16,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
