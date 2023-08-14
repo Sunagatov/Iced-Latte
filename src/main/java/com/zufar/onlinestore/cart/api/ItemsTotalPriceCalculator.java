6f6d4276c1bc9c7d08382112c0ee271e60073281
Reyzis2021 <reyzarium@gmail.com>
2023-08-14 13:31:09 +0400

    Refactored during review comments

--- src/main/java/com/zufar/onlinestore/cart/api/ItemsTotalPriceCalculator.java
+++ src/main/java/com/zufar/onlinestore/cart/api/ItemsTotalPriceCalculator.java
@@ -1,17 +1,14 @@
 package com.zufar.onlinestore.cart.api;
 
 import com.zufar.onlinestore.cart.dto.ShoppingSessionItemDto;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
 import java.math.BigDecimal;
-import java.util.Collection;
+import java.util.List;
 
-@Slf4j
 @Service
 public class ItemsTotalPriceCalculator {
 
-    public BigDecimal calculate(Collection<ShoppingSessionItemDto> items) {
+    public BigDecimal calculate(List<ShoppingSessionItemDto> items) {
         return items.stream()
                 .map(item -> item.productInfo().price()
                         .multiply(BigDecimal.valueOf(item.productsQuantity())))
