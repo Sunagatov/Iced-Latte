7c7812a61723ae9f2ad32e8b90348a99c68117d9
Vitalii <vitaliibredun@gmail.com>
2023-08-23 21:43:05 +0300

    fix: changed the UserDtoConverter

--- src/main/java/com/zufar/onlinestore/user/converter/UserDtoConverter.java
+++ src/main/java/com/zufar/onlinestore/user/converter/UserDtoConverter.java
@@ -1,12 +1,20 @@
 package com.zufar.onlinestore.user.converter;
 
 import com.zufar.onlinestore.user.dto.UserDto;
+import com.zufar.onlinestore.user.entity.Authority;
 import com.zufar.onlinestore.user.entity.UserEntity;
+import com.zufar.onlinestore.user.entity.UserGrantedAuthority;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.Collections;
+import java.util.Set;
 
 @Mapper(componentModel = "spring", uses = AddressDtoConverter.class)
 public interface UserDtoConverter {
+
+    AddressDtoConverter addressConverter = Mappers.getMapper(AddressDtoConverter.class);
 
     @Mapping(target = "address", source = "entity.address", qualifiedByName = "toAddressDto")
     UserDto toDto(final UserEntity entity);
@@ -16,13 +24,31 @@ public interface UserDtoConverter {
     @Mapping(target = "credentialsNonExpired", constant = "true")
     @Mapping(target = "enabled", constant = "true")
     @Mapping(target = "address", source = "dto.address", qualifiedByName = "toAddress")
-    @Mapping(target = "authorities",
-            expression = "java(java.util.Collections.singleton(com.zufar.onlinestore.user.entity.UserGrantedAuthority" +
-                    ".builder().authority(com.zufar.onlinestore.user.entity.Authority.USER)" +
-                    ".user(com.zufar.onlinestore.user.entity.UserEntity" +
-                    ".builder().firstName(dto.firstName()).lastName(dto.lastName()).email(dto.email())" +
-                    ".username(dto.username()).password(dto.password())" +
-                    ".address(new AddressDtoConverterImpl().toEntity((dto.address()))).accountNonExpired(true)" +
-                    ".accountNonLocked(true).credentialsNonExpired(true).enabled(true).build()).build()))")
+    @Mapping(target = "authorities", expression = "java(createAuthorities(dto))")
     UserEntity toEntity(final UserDto dto);
+
+    default Set<UserGrantedAuthority> createAuthorities(UserDto dto) {
+        UserEntity userEntity = UserEntity.builder()
+                .firstName(dto.firstName())
+                .lastName(dto.lastName())
+                .email(dto.email())
+                .username(dto.username())
+                .password(dto.password())
+                .address(addressConverter.toEntity((dto.address())))
+                .accountNonExpired(true)
+                .accountNonLocked(true)
+                .credentialsNonExpired(true)
+                .enabled(true)
+                .build();
+
+        Set<UserGrantedAuthority> authorities = Collections
+                .singleton(UserGrantedAuthority.builder()
+                        .authority(Authority.USER)
+                        .user(userEntity)
+                        .build());
+
+        userEntity.setAuthorities(authorities);
+
+        return authorities;
+    }
 }
