edf159ab735df77781e08e0d2a1a7a03517e39e6
zufar-sunagatov <zufar.sunagatov@gmail.com>
2023-09-24 13:08:43 +0100

    Unused imports and extra spaces were deleted

--- src/main/java/com/zufar/onlinestore/common/validation/email/UniqueEmail.java
+++ src/main/java/com/zufar/onlinestore/common/validation/email/UniqueEmail.java
@@ -1,6 +1,5 @@
 package com.zufar.onlinestore.common.validation.email;
 
-import com.zufar.onlinestore.common.validation.email.UniqueEmailValidator;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
@@ -11,7 +10,7 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Target({ FIELD })
+@Target({FIELD})
 @Retention(RUNTIME)
 @Constraint(validatedBy = UniqueEmailValidator.class)
 @Documented
@@ -19,8 +18,8 @@ public @interface UniqueEmail {
 
     String message() default "";
 
-    Class<?>[] groups() default { };
+    Class<?>[] groups() default {};
 
-    Class<? extends Payload>[] payload() default { };
+    Class<? extends Payload>[] payload() default {};
 
 }
--- src/main/java/com/zufar/onlinestore/common/validation/username/UniqueUsername.java
+++ src/main/java/com/zufar/onlinestore/common/validation/username/UniqueUsername.java
@@ -6,10 +6,11 @@ import jakarta.validation.Payload;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Target({ FIELD })
+@Target({FIELD})
 @Retention(RUNTIME)
 @Constraint(validatedBy = UniqueUsernameValidator.class)
 @Documented
@@ -17,7 +18,7 @@ public @interface UniqueUsername {
 
     String message() default "";
 
-    Class<?>[] groups() default { };
+    Class<?>[] groups() default {};
 
-    Class<? extends Payload>[] payload() default { };
+    Class<? extends Payload>[] payload() default {};
 }
