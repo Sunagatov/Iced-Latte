a69af5a61d68840a0670071d63c8a9a2b6a03815
zufar-sunagatov <zufar.sunagatov@gmail.com>
2023-09-24 19:50:13 +0100

    Added new validation constraints to UserAuthenticationRequest

--- src/main/java/com/zufar/onlinestore/security/dto/UserAuthenticationRequest.java
+++ src/main/java/com/zufar/onlinestore/security/dto/UserAuthenticationRequest.java
@@ -1,16 +1,13 @@
 package com.zufar.onlinestore.security.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 public record UserAuthenticationRequest(
 
         @NotBlank(message = "Username is the mandatory attribute")
-        @Size(max = 55, message = "Username length must be less than 55 characters")
         String username,
 
         @NotBlank(message = "Password is the mandatory attribute")
-        @Size(max = 55, message = "Password length must be less than 55 characters")
         String password
 ) {
 }
--- src/main/java/com/zufar/onlinestore/security/dto/UserRegistrationRequest.java
+++ src/main/java/com/zufar/onlinestore/security/dto/UserRegistrationRequest.java
@@ -5,30 +5,35 @@ import com.zufar.onlinestore.common.validation.username.UniqueUsername;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 
 
 public record UserRegistrationRequest(
 
-        @NotBlank(message = "FirstName is the mandatory attribute")
-        @Size(max = 55, message = "FirstName length must be less than 55 characters")
+        @NotBlank(message = "First name is the mandatory attribute")
+        @Size(min = 2, max = 128, message = "First name should have a length between 2 and 128 characters")
+        @Pattern(regexp = "^(?!\\s)(?!.*\\s$)(?!.*?--)[A-Za-z\\s-]*$", message = "First name can only contain Latin letters, spaces, and hyphens")
         String firstName,
 
-        @NotBlank(message = "LastName is the mandatory attribute")
-        @Size(max = 55, message = "LastName length must be less than 55 characters")
+        @NotBlank(message = "Last name is the mandatory attribute")
+        @Size(min = 2, max = 128, message = "Last name should have a length between 2 and 128 characters")
+        @Pattern(regexp = "^(?!\\s)(?!.*\\s$)(?!.*?--)[A-Za-z\\s-]*$", message = "Last name can only contain Latin letters, spaces, and hyphens")
         String lastName,
 
         @UniqueUsername(message = "Username must be unique")
         @NotBlank(message = "Username is the mandatory attribute")
-        @Size(max = 55, message = "Username length must be less than 55 characters")
+        @Size(min = 2, max = 128, message = "Username should have a length between 2 and 128 characters")
+        @Pattern(regexp = "^(?!\\s)(?!.*\\s$)(?!.*?--)[A-Za-z0-9@._\\-@$!%*?&]*$", message = "Username can only contain latin letters, digits, and may include special characters @$!%*?&")
         String username,
 
         @UniqueEmail(message = "Email must be unique")
         @Email(message = "Email must be valid")
-        @NotBlank(message = "Email is the mandatory attribute")
+        @NotBlank(message = "Email should have a length between 2 and 128 characters")
         String email,
 
         @NotBlank(message = "Password is the mandatory attribute")
-        @Size(max = 55, message = "Password length must be less than 55 characters")
+        @Size(min = 8, max = 128, message = "Password should have a length between 8 and 128 characters")
+        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d@$!%*?&]{8,}$", message = "Password must be at least 8 characters long and contain at least one letter, one digit, and may include special characters @$!%*?&")
         String password
 ) {
 }
