789ac4275a46fa69abb2b5bf6c294e6fb50ae657
zufar-sunagatov <zufar.sunagatov@gmail.com>
2023-09-24 17:37:50 +0100

    Validation annotations were deleted in UserSecurityEndpoint

--- src/main/java/com/zufar/onlinestore/security/endpoint/UserSecurityEndpoint.java
+++ src/main/java/com/zufar/onlinestore/security/endpoint/UserSecurityEndpoint.java
@@ -1,14 +1,13 @@
 package com.zufar.onlinestore.security.endpoint;
 
+import com.zufar.onlinestore.openapi.security.api.SecurityApi;
 import com.zufar.onlinestore.security.api.UserSecurityManager;
-import com.zufar.onlinestore.security.dto.authentication.UserAuthenticationRequest;
-import com.zufar.onlinestore.security.dto.authentication.UserAuthenticationResponse;
-import com.zufar.onlinestore.security.dto.registration.UserRegistrationRequest;
-import com.zufar.onlinestore.security.dto.registration.UserRegistrationResponse;
+import com.zufar.onlinestore.security.dto.UserAuthenticationRequest;
+import com.zufar.onlinestore.security.dto.UserAuthenticationResponse;
+import com.zufar.onlinestore.security.dto.UserRegistrationRequest;
+import com.zufar.onlinestore.security.dto.UserRegistrationResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -26,30 +25,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserSecurityEndpoint implements SecurityApi {
 
-	public static final String USER_SECURITY_API_URL = "/api/v1/auth/";
+    public static final String USER_SECURITY_API_URL = "/api/v1/auth/";
 
-	private final UserSecurityManager userSecurityManager;
+    private final UserSecurityManager userSecurityManager;
 
-	@Override
-	@PostMapping("/register")
-	public ResponseEntity<UserRegistrationResponse> register(@RequestBody @NotNull final UserRegistrationRequest request) {
-		UserRegistrationResponse authenticationResponse = userSecurityManager.register(request);
-		return new ResponseEntity<>(authenticationResponse, HttpStatus.CREATED);
-	}
+    @Override
+    @PostMapping("/register")
+    public ResponseEntity<UserRegistrationResponse> register(@RequestBody final UserRegistrationRequest request) {
+        UserRegistrationResponse authenticationResponse = userSecurityManager.register(request);
+        return new ResponseEntity<>(authenticationResponse, HttpStatus.CREATED);
+    }
 
-	@Override
-	@PostMapping("/authenticate")
-	public ResponseEntity<UserAuthenticationResponse> authenticate(@RequestBody @NotNull final UserAuthenticationRequest request) {
-		UserAuthenticationResponse authenticationResponse = userSecurityManager.authenticate(request);
-		return ResponseEntity
-				.ok(authenticationResponse);
-	}
+    @Override
+    @PostMapping("/authenticate")
+    public ResponseEntity<UserAuthenticationResponse> authenticate(@RequestBody final UserAuthenticationRequest request) {
+        UserAuthenticationResponse authenticationResponse = userSecurityManager.authenticate(request);
+        return ResponseEntity
+                .ok(authenticationResponse);
+    }
 
-	@PostMapping("/logout")
-	public ResponseEntity<Void> logout(final HttpServletRequest request,
-									   final HttpServletResponse response) {
-		userSecurityManager.logout(request, response);
-		return ResponseEntity.ok()
-				.build();
-	}
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(final HttpServletRequest request,
+                                       final HttpServletResponse response) {
+        userSecurityManager.logout(request, response);
+        return ResponseEntity.ok()
+                .build();
+    }
 }
