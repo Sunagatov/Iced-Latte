dccb294d9dec905e6ea39a5329d32177c42625bd
zufar-sunagatov <zufar.sunagatov@gmail.com>
2023-11-08 21:30:38 +0000

    new hardcoded products to the database were added

--- src/main/resources/db/changelog/version-1.0/19.07.2023.part2.insert-product-data.sql
+++ src/main/resources/db/changelog/version-1.0/19.07.2023.part2.insert-product-data.sql
@@ -15,3 +15,65 @@ VALUES ('46f97165-00a7-4b45-9e5c-09f8168b0047', 'Macchiato', 'Espresso with a Da
 
 INSERT INTO product (id, name, description, price, quantity, active)
 VALUES ('e6a4d7f2-d40e-4e5f-93b8-5d56ce6724c5', 'Americano', 'Espresso Diluted with Hot Water', 4.49, 70, true);
+
+INSERT INTO product (id, name, description, price, quantity, active)
+VALUES ('fa1e12ff-67e4-42d5-bf45-c43576890f8a', 'Flat White', 'Smooth Espresso with Velvety Milk', 5.99, 50, true);
+
+INSERT INTO product (id, name, description, price, quantity, active)
+VALUES ('6d77f8a9-e640-4d2e-ba2c-b7db8ab2c123', 'Iced Coffee', 'Chilled Espresso with Milk and Ice', 4.49, 100, true);
+
+INSERT INTO product (id, name, description, price, quantity, active)
+VALUES ('ba5f15c4-1f72-4b97-b9cf-4437e5c6c2fa', 'Affogato', 'Espresso Poured over Vanilla Ice Cream', 6.49, 30, true);
+
+INSERT INTO product (id, name, description, price, quantity, active)
+VALUES ('3e9c1f94-ee3c-4e0b-9a6e-bb6e9c61c6f5', 'Cortado', 'Equal Parts Espresso and Steamed Milk', 3.99, 80, true);
+
+INSERT INTO product (id, name, description, price, quantity, active)
+VALUES ('3ea8e601-24c9-49b1-8c65-8db8b3a5c7a3', 'Cold Brew', 'Slow-Steeped, Cold Coffee Drink', 4.99, 110, true);
+
+INSERT INTO product (id, name, description, price, quantity, active)
+VALUES ('eec8a1d8-4864-4c1b-aa8b-dedfddc6e356', 'Nitro Coffee', 'Cold Brew Coffee with Nitrogen', 5.49, 75, true);
+
+INSERT INTO product (id, name, description, price, quantity, active)
+VALUES ('7edf3a1e-c391-4d76-9002-2f0dd3e9c6e9', 'Frappuccino', 'Blended Ice with Coffee and Milk', 6.99, 65, true);
+
+INSERT INTO product (id, name, description, price, quantity, active)
+VALUES ('e0323d1b-1169-4a0e-8d7b-07ff3bfe7f7e', 'Turkish Coffee', 'Finely Ground Coffee Brewed in a Pot', 4.49, 40, true);
+
+INSERT INTO product (id, name, description, price, quantity, active)
+VALUES ('e70f1d94-d55f-4e0e-8a6b-28e2ca3c6c34', 'Red Eye', 'Drip Coffee with an Espresso Shot', 5.99, 85, true);
+
+INSERT INTO product (id, name, description, price, quantity, active)
+VALUES ('b5faee5d-6e6d-4319-ba9f-8d1bf7ee3f63', 'Chai Latte', 'Spiced Tea with Steamed Milk', 5.29, 60, true);
+
+INSERT INTO product (id, name, description, price, quantity, active)
+VALUES ('25a8e8c1-37ba-4a8b-927f-5f1b4b5b5c3c', 'Green Tea Latte', 'Matcha Green Tea with Steamed Milk', 4.95, 70, true);
+
+INSERT INTO product (id, name, description, price, quantity, active)
+VALUES ('4e9a7d28-5e40-4b14-bc72-a5d1b547c3d0', 'Hot Chocolate', 'Rich Milk Chocolate with Whipped Cream', 3.75, 100, true);
+
+INSERT INTO product (id, name, description, price, quantity, active)
+VALUES ('cc0b6e5d-71f1-44cd-8a5b-1bdb978b5ca6', 'Iced Latte', 'Chilled Espresso with Cold Milk', 4.50, 85, true);
+
+INSERT INTO product (id, name, description, price, quantity, active)
+VALUES ('aa1d3e8f-9866-4e07-bc61-b5d8c2a3df4b', 'Pumpkin Spice Latte', 'Seasonal Spiced Latte with Pumpkin Flavor', 5.25, 45, true);
+
+INSERT INTO product (id, name, description, price, quantity, active)
+VALUES ('eedc6cde-1e80-4ebf-a9d1-8e5e4eb2cacf', 'Iced Macchiato', 'Espresso with Milk and Ice', 3.95, 90, true);
+
+INSERT INTO product (id, name, description, price, quantity, active)
+VALUES ('fc88cd5d-5049-4b00-8d88-df1d9b4a3ce1', 'Vanilla Latte', 'Espresso with Vanilla and Steamed Milk', 4.85, 60, true);
+
+INSERT INTO product (id, name, description, price, quantity, active)
+VALUES ('5efb7cfd-744b-4af9-b713-ef8d30abf628', 'Caramel Macchiato', 'Espresso with Caramel and Frothy Milk', 5.75, 50, true);
+
+INSERT INTO product (id, name, description, price, quantity, active)
+VALUES ('b183776e-6c3b-459a-bb3a-e93a4c8e4e56', 'Peppermint Mocha', 'Chocolate Espresso with Peppermint Flavor', 5.50, 55, true);
+
+INSERT INTO product (id, name, description, price, quantity, active)
+VALUES ('c3f45eec-18d8-43e0-9d7b-d85a4a9b6bda', 'Hazelnut Latte', 'Espresso with Hazelnut Flavor and Milk', 4.75, 75, true);
+
+INSERT INTO product (id, name, description, price, quantity, active)
+VALUES ('123f7a2d-cb34-4e5f-9a1d-4e4b456a03a7', 'Lemonade Iced Tea', 'Iced Tea with a Splash of Lemonade', 3.65, 80, true);
+
+
