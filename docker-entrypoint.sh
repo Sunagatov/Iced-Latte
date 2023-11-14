4bfd15f7d3d9b2b73ee93f287fd7620122e2e1c0
zufar-sunagatov <zufar.sunagatov@gmail.com>
2023-11-14 01:08:34 +0000

    docker-entrypoint.sh was updated

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,24 +1,2 @@
 #!/bin/sh
-set -e
-
-host="${DATASOURCE_HOST:-postgresdb}"
-max_retries=3
-wait_seconds=2
-
-retries=0
-until nc -z "$host" "$DATASOURCE_PORT" || [ $retries -eq "$max_retries" ]
-do
-  retries=$((retries+1))
-  echo "Waiting for postgres database at $host:$DATASOURCE_PORT... ($retries/$max_retries)"
-  sleep $((wait_seconds))
-  wait_seconds=$((wait_seconds*2))
-done
-
-if [ $retries -eq "$max_retries" ]
-then
-  echo "Failed to connect to postgres database at $host:$DATASOURCE_PORT after $max_retries retries."
-  exit 1
-fi
-
-echo "PostgresDB at $host:$DATASOURCE_PORT is up - executing command"
 exec java -jar /usr/app/app.jar
