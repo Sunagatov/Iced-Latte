86f1749fcb9eb0c3fbc5b7bbd3047eda254cd6bc
zufar-sunagatov <zufar.sunagatov@gmail.com>
2023-11-26 00:40:22 +0000

    Local Monitoring (Grafana Prometheus) was added

---
+++ src/main/java/com/zufar/icedlatte/security/configuration/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.zufar.icedlatte.security.configuration;
+
+public final class SecurityConstants {
+    public static final String SHOPPING_CART_URL = "/api/v1/cart/**";
+    public static final String PAYMENT_URL = "/api/v1/payment/**";
+    public static final String USERS_URL = "/api/v1/users/**";
+
+    private SecurityConstants() {}
+}
