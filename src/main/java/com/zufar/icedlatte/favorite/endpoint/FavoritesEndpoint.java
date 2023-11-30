3d562bd7b495216c8224dc7c54741f1d24b07fa2
alexandr <matcnbev2001@mail.ru>
2023-11-29 22:24:12 -0500

    open import depends in endpoint

--- src/main/java/com/zufar/icedlatte/favorite/endpoint/FavoritesEndpoint.java
+++ src/main/java/com/zufar/icedlatte/favorite/endpoint/FavoritesEndpoint.java
@@ -13,7 +13,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.UUID;
 
