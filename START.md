6a42961ba5b573782a81e36c1c826b35a87d3735
zufar-sunagatov <zufar.sunagatov@gmail.com>
2023-12-16 01:54:51 +0000

    changes

--- START.md
+++ START.md
@@ -26,7 +26,7 @@ Key variables which are used in the startup of the app. They are pre-configured 
 ### Running All Services with latest build
 To start the application containers with the latest build:
 ```bash
-docker-compose -f docker-compose.local.yml up -d --build iced-latte-backend iced-latte-postgresdb
+docker-compose -f docker-compose.local.yml up -d --build
 ```
 
 ### Running All Services
--- src/main/java/com/zufar/icedlatte/common/config/MinioConfig.java
+++ src/main/java/com/zufar/icedlatte/common/config/MinioConfig.java
@@ -1,14 +1,10 @@
 package com.zufar.icedlatte.common.config;
 
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import io.minio.BucketExistsArgs;
-import io.minio.MakeBucketArgs;
-import io.minio.MinioClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -20,9 +16,6 @@ public class MinioConfig {
 
     @Value("${spring.minio.url}")
     private String minioUrl;
-
-    @Value("${spring.minio.secure}")
-    private boolean minioSecure;
 
     @Value("${spring.minio.access-key}")
     private String minioAccessKey;
@@ -38,14 +31,17 @@ public class MinioConfig {
 
     @Bean
     public AmazonS3 amazonS3() {
-        AWSCredentials awsCredentials = new BasicAWSCredentials(minioAccessKey, minioSecretKey);
         AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
                 .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(minioUrl, region))
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(minioAccessKey, minioSecretKey)))
                 .build();
 
-        if (!amazonS3.doesBucketExistV2(minioAvatarBucket)) {
-            amazonS3.createBucket(minioAvatarBucket);
+        try {
+            if (!amazonS3.doesBucketExistV2(minioAvatarBucket)) {
+                amazonS3.createBucket(minioAvatarBucket);
+            }
+        } catch (Exception exception) {
+            log.error("Creating AmazonS3 bucket was failed", exception);
         }
 
         return amazonS3;
