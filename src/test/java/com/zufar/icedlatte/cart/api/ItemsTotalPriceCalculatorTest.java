baeaff8eda894223b36fadb5b1ea7686fc141cf6
yevr19 <a0040@ukr.net>
2023-12-23 08:47:11 -0800

    IL-148: Order Details on Checkout Page

--- src/test/java/com/zufar/icedlatte/cart/api/ItemsTotalPriceCalculatorTest.java
+++ src/test/java/com/zufar/icedlatte/cart/api/ItemsTotalPriceCalculatorTest.java
@@ -15,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(MockitoExtension.class)
 class ItemsTotalPriceCalculatorTest {
 
-    static final BigDecimal TOTAL_PRICE_FOR_SHOPPING_SESSION = BigDecimal.valueOf(15.4);
+    static final BigDecimal TOTAL_PRICE_FOR_SHOPPING_SESSION = new BigDecimal("21.170");
 
     @InjectMocks
     private ItemsTotalPriceCalculator itemsTotalPriceCalculator;
