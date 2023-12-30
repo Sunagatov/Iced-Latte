40a2e7b11be9f87e092740d069e429a5f36dfb61
Zufar Sunagatov <zufar.sunagatov@gmail.com>
2023-12-30 23:10:36 +0300

    refactoring

--- src/main/java/com/zufar/icedlatte/ApplicationMigration.java
+++ src/main/java/com/zufar/icedlatte/ApplicationMigration.java
@@ -5,6 +5,7 @@ import com.zufar.icedlatte.filestorage.filemetadata.FileMetadataSaver;
 import com.zufar.icedlatte.filestorage.dto.FileMetadataDto;
 import com.zufar.icedlatte.filestorage.minio.MinioProvider;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -12,14 +13,16 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ApplicationMigration implements ApplicationRunner {
 
-    private final static String DIRECTORY_PATH = "./products";
-
     @Value("${spring.minio.buckets.product-picture}")
     private String productPictureBucket;
+
+    @Value("${spring.minio.default-image-directory.products}")
+    private String directoryPath;
 
     private final FileUploader fileUploader;
     private final MinioProvider minioProvider;
@@ -27,8 +30,14 @@ public class ApplicationMigration implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        fileUploader.uploadDirectory(productPictureBucket, DIRECTORY_PATH);
+        log.info("Product pictures upload was started");
+        fileUploader.uploadDirectory(productPictureBucket, directoryPath);
+        Thread.sleep(5000);
+        log.info("Product pictures upload was finished");
         List<FileMetadataDto> fileMetadataDtos = minioProvider.getProductImagesFromMinio(productPictureBucket);
+        log.info("Product pictures metadata was retrieved from minio");
         fileMetadataSaver.saveAll(fileMetadataDtos);
+        log.info("Product pictures metadata was saved in postgresql database");
+
     }
 }
