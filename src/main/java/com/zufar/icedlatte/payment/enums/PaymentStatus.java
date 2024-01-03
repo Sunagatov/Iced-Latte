95b9e39cb19cfc067477154a1a69d9e1d65f897f
alexandr <matcnbev2001@mail.ru>
2024-01-03 03:48:52 -0500

    update enums

--- src/main/java/com/zufar/icedlatte/payment/enums/PaymentStatus.java
+++ src/main/java/com/zufar/icedlatte/payment/enums/PaymentStatus.java
@@ -8,28 +8,28 @@ import lombok.RequiredArgsConstructor;
 public enum PaymentStatus {
 
     PAYMENT_METHOD_IS_REQUIRED("PAYMENT_METHOD_IS_REQUIRED",
-            "PAYMENT_METHOD_IS_REQUIRED"),
+            "Payment method is required."),
 
     PAYMENT_CONFIRMATION_IS_REQUIRED("PAYMENT_CONFIRMATION_IS_REQUIRED",
-            "PAYMENT_CONFIRMATION_IS_REQUIRED"),
+            "Payment confirmation is required."),
 
     PAYMENT_CAPTURE_IS_REQUIRED("PAYMENT_CAPTURE_IS_REQUIRED",
-            "PAYMENT_CAPTURE_IS_REQUIRED"),
+            "Payment capture is required."),
 
     PAYMENT_ACTION_IS_REQUIRED("PAYMENT_ACTION_IS_REQUIRED",
-            "PAYMENT_ACTION_IS_REQUIRED"),
+            "Payment action is required."),
 
     PAYMENT_IS_FAILED("PAYMENT_IS_FAILED",
-            "PAYMENT_IS_FAILED"),
+            "Payment has failed."),
 
-    PAYMENT_IN_PROCESSING("PAYMENT_IN_PROCESSING",
-            "PAYMENT_IN_PROCESSING"),
+    PAYMENT_IN_PROCESSING("IN_PROCESSING",
+            "Payment is in processing."),
 
     PAYMENT_IS_CANCELED("PAYMENT_IS_CANCELED",
-            "PAYMENT_IS_CANCELED"),
+            "Payment has been canceled."),
 
     PAYMENT_IS_SUCCEEDED("PAYMENT_IS_SUCCEEDED",
-            "PAYMENT_IS_SUCCEEDED");
+            "Payment has succeeded.");
 
     private final String status;
     private final String description;
