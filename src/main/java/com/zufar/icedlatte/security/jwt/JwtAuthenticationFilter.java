3c26d1c5ab8885b7754830736892ad54386c6070
MatsnevAl <al.matsnev.01@gmail.com>
2024-02-17 11:03:45 +0300

    tests fix

--- src/main/java/com/zufar/icedlatte/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/zufar/icedlatte/security/jwt/JwtAuthenticationFilter.java
@@ -93,7 +93,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         return Stream.of(SecurityConstants.SHOPPING_CART_URL, SecurityConstants.PAYMENT_URL,
                         SecurityConstants.USERS_URL, SecurityConstants.FAVOURITES_URL,
                         SecurityConstants.AUTH_URL, SecurityConstants.ORDERS_URL,
-                        SecurityConstants.PRODUCT_URL)
+                        SecurityConstants.RATING_URL)
                 .anyMatch(securedUrl -> new AntPathRequestMatcher(securedUrl).matches(request));
     }
 }
