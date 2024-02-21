1c2159a513a202a8e17627284b63e3c4c6a4c915
annstriganova <20650874+annstriganova@users.noreply.github.com>
2024-02-21 20:25:36 +0100

    added getProductReviews stub

--- src/main/java/com/zufar/icedlatte/review/api/PageableReviewsProvider.java
+++ src/main/java/com/zufar/icedlatte/review/api/PageableReviewsProvider.java
@@ -1,10 +1,6 @@
 package com.zufar.icedlatte.review.api;
 
 import com.zufar.icedlatte.openapi.dto.ProductReviewsAndRatingsWithPagination;
-import com.zufar.icedlatte.product.repository.ProductInfoRepository;
-import com.zufar.icedlatte.review.converter.ProductReviewDtoConverter;
-import com.zufar.icedlatte.review.repository.ReviewRepository;
-import com.zufar.icedlatte.security.api.SecurityPrincipalProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
@@ -21,12 +17,6 @@ import static com.zufar.icedlatte.common.util.Utils.createPageableObject;
 @Service
 @RequiredArgsConstructor
 public class PageableReviewsProvider {
-
-    private final ReviewRepository reviewRepository;
-    private final ProductInfoRepository productInfoRepository;
-    private final ProductReviewDtoConverter productReviewDtoConverter;
-    private final SecurityPrincipalProvider securityPrincipalProvider;
-
     @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.READ_COMMITTED, readOnly = true)
     public ProductReviewsAndRatingsWithPagination getProductReviews(final UUID productId, final Integer page,
                                                                     final Integer size,
@@ -34,7 +24,7 @@ public class PageableReviewsProvider {
                                                                     final String sortDirection) {
         Pageable pageable = createPageableObject(page, size, sortAttribute, sortDirection);
 
-        return null;
+        throw new UnsupportedOperationException("getProductReviews has not been implemenetd");
     }
 
 }
