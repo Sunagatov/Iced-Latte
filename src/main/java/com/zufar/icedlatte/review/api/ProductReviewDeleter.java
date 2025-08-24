8dd873e4a2a783e1151372d52d9ebf4516fc10b4
zufar-sunagatov <zufar.sunagatov@gmail.com>
2025-08-24 20:26:40 +0100

    cache eviction for products were added

--- src/main/java/com/zufar/icedlatte/review/api/ProductReviewDeleter.java
+++ src/main/java/com/zufar/icedlatte/review/api/ProductReviewDeleter.java
@@ -5,6 +5,7 @@ import com.zufar.icedlatte.review.repository.ProductReviewRepository;
 import com.zufar.icedlatte.review.validator.ProductReviewValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
@@ -22,6 +23,7 @@ public class ProductReviewDeleter {
     private final ProductInfoRepository productInfoRepository;
 
     @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.READ_COMMITTED)
+    @CacheEvict(cacheNames = "productById", key = "#productId")
     public void delete(final UUID productId,
                        final UUID productReviewId) {
         productReviewValidator.validateProductReviewDeletionAllowed(productReviewId);
