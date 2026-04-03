98fa51d4dc4bd4916884094fec7b837b9646e606
zufar-sunagatov <zufar.sunagatov@gmail.com>
2026-04-03 01:07:37 +0100

    bugs were fixed: - favourites and shopping cart sync in guest mode - reset, change user's password - avatar upload - product review - some other small bugs

--- src/test/java/com/zufar/icedlatte/user/api/SingleUserProviderTest.java
+++ src/test/java/com/zufar/icedlatte/user/api/SingleUserProviderTest.java
@@ -15,7 +15,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
