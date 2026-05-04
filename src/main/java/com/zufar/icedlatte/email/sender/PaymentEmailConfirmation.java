d94fc9691596ed908ee89a57d3c0358d00786e52
zufar-sunagatov <zufar.sunagatov@gmail.com>
2026-05-04 02:30:40 +0100

    update

--- src/main/java/com/zufar/icedlatte/email/sender/PaymentEmailConfirmation.java
+++ src/main/java/com/zufar/icedlatte/email/sender/PaymentEmailConfirmation.java
@@ -6,11 +6,13 @@ import com.zufar.icedlatte.email.message.MessageBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@ConditionalOnProperty(name = "email.enabled", havingValue = "true")
 public class PaymentEmailConfirmation extends AbstractEmailSender<EmailConfirmMessage> {
 
     private static final String DEFAULT_SUCCESSFUL_EMAIL_MESSAGE = "Your payment with total amount - %.2f %s was successfully processed";
