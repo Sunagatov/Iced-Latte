84fdcfad88a8b6b1702a147c20768899b892e208
zufar-sunagatov <zufar.sunagatov@gmail.com>
2026-05-22 00:59:46 +0100

    Spring Modulith improvements

--- src/main/java/com/zufar/icedlatte/filestorage/repository/FileMetadataRepository.java
+++ src/main/java/com/zufar/icedlatte/filestorage/repository/FileMetadataRepository.java
@@ -1,7 +1,7 @@
 package com.zufar.icedlatte.filestorage.repository;
 
 import com.zufar.icedlatte.filestorage.entity.FileMetadata;
-import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
