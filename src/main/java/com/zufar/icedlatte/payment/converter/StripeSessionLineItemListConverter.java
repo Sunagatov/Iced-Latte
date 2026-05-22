eb5c360c71c8f627f436a8a27f14bc1a190e3c6b
zufar-sunagatov <zufar.sunagatov@gmail.com>
2026-05-22 21:34:11 +0100

    robustness improvements

--- src/main/java/com/zufar/icedlatte/payment/converter/StripeSessionLineItemListConverter.java
+++ src/main/java/com/zufar/icedlatte/payment/converter/StripeSessionLineItemListConverter.java
@@ -4,6 +4,7 @@ import com.stripe.param.checkout.SessionCreateParams;
 import com.zufar.icedlatte.cart.api.dto.CartItemSnapshot;
 import com.zufar.icedlatte.order.api.OrderSnapshot;
 import com.zufar.icedlatte.payment.config.StripeProperties;
+import org.jspecify.annotations.NonNull;
 import org.mapstruct.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -12,20 +13,22 @@ import java.math.RoundingMode;
 import java.util.List;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING,
-        imports = BigDecimal.class, unmappedTargetPolicy = ReportingPolicy.IGNORE)
-@SuppressWarnings("unused") // MapStruct generates and calls the implementation.
+        imports = BigDecimal.class,
+        unmappedTargetPolicy = ReportingPolicy.ERROR)
+@SuppressWarnings({"unused", "NullableProblems"}) // MapStruct generates and calls the implementation.
 public abstract class StripeSessionLineItemListConverter {
 
     @Autowired
     protected StripeProperties stripeProperties;
 
-    public abstract List<SessionCreateParams.LineItem> toLineItems(List<CartItemSnapshot> shoppingCartItems);
+    public abstract List<SessionCreateParams.LineItem> toLineItems(@NonNull List<CartItemSnapshot> shoppingCartItems);
 
     @Mapping(target = "priceData.unitAmount", source = "product.price", qualifiedByName = "toStripeUnitAmount")
     @Mapping(target = "priceData.currency", expression = "java(stripeProperties.currency())")
     @Mapping(target = "priceData.productData.name", source = "product.name")
     @Mapping(target = "quantity", source = "productQuantity")
-    public abstract SessionCreateParams.LineItem toLineItem(CartItemSnapshot shoppingCartItem);
+    @BeanMapping(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+    public abstract SessionCreateParams.LineItem toLineItem(@NonNull CartItemSnapshot shoppingCartItem);
 
     public SessionCreateParams.LineItem toLineItem(OrderSnapshot.OrderItemSnapshot item) {
         return SessionCreateParams.LineItem.builder()
