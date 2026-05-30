f688796c7b0d214e24a1171b668d64b13612f735
zufar-sunagatov <zufar.sunagatov@gmail.com>
2026-05-30 23:20:05 +0100

    update

--- src/main/java/com/zufar/icedlatte/product/service/ProductService.java
+++ src/main/java/com/zufar/icedlatte/product/service/ProductService.java
@@ -5,7 +5,6 @@ import static com.zufar.icedlatte.product.specification.ProductSpecifications.*;
 import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -159,9 +158,6 @@ public class ProductService implements ProductCatalogApi {
     }
 
     private static void validateProductIds(List<UUID> ids) {
-        if (ids.stream().anyMatch(Objects::isNull)) {
-            throw new BadRequestException("Product ids must not contain null values.");
-        }
         if (new HashSet<>(ids).size() != ids.size()) {
             throw new BadRequestException("Product ids must not contain duplicate values.");
         }
