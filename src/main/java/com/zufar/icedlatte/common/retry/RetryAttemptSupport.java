7a584e862db7f5a0c51cb9dbc4e68f0ebabb119d
zufar-sunagatov <zufar.sunagatov@gmail.com>
2026-06-06 12:51:03 +0100

    RetryAttemptSupport was added

---
+++ src/main/java/com/zufar/icedlatte/common/retry/RetryAttemptSupport.java
@@ -0,0 +1,28 @@
+package com.zufar.icedlatte.common.retry;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class RetryAttemptSupport {
+
+    private static final int MAX_ERROR_LENGTH = 1000;
+    private static final int MAX_BACKOFF_EXPONENT = 8;
+    private static final long MAX_BACKOFF_SECONDS = 300L;
+
+    public static Instant nextAttemptAt(int attemptCount) {
+        return Instant.now().plus(backoffSeconds(attemptCount), ChronoUnit.SECONDS);
+    }
+
+    public static long backoffSeconds(int attemptCount) {
+        long backoff = attemptCount > MAX_BACKOFF_EXPONENT ? MAX_BACKOFF_SECONDS : 1L << attemptCount;
+        return Math.min(backoff, MAX_BACKOFF_SECONDS);
+    }
+
+    public static String sanitizedError(Throwable failure) {
+        String message = failure.getClass().getSimpleName() + ": " + failure.getMessage();
+        return message.length() <= MAX_ERROR_LENGTH ? message : message.substring(0, MAX_ERROR_LENGTH);
+    }
+}
---
+++ src/main/java/com/zufar/icedlatte/common/retry/package-info.java
@@ -0,0 +1,2 @@
+@org.springframework.modulith.NamedInterface("retry")
+package com.zufar.icedlatte.common.retry;
